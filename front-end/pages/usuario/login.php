b343013e19dcecf9a799c4334466f4ca88042462
HerosAugustoBaiaOliveira <herosbaia2006@gmail.com>
2025-02-03 10:45:38 -0300

    pequenas alteraçoes

--- front-end/pages/usuario/login.php
+++ front-end/pages/usuario/login.php
@@ -1,36 +1,37 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+        <!DOCTYPE html>
+        <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/style.css">
-    <title>Login</title>
-</head>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="../../css/style.css">
+            <title>Login</title>
+        </head>
 
-<body>
-    <header>
-        <?php
-            include "../../../inc/cabecalho.php"
-                
+        <body>
+            <header>
+                <?php
+                    include "../../../inc/cabecalho.php"
 
-        ?>
-    </header>
+                        
 
-    <main>
-         <form action="../../../back-end/verificacaoLogin.php" method="get">
-            <label for="name">Nome:</label>
-            <input type="text" name="name" id="name">
-            <input type="submit" value="Enviar">
-        </form>
-    </main>
+                ?>
+            </header>
 
-    <footer>
-        <?php
-             include "../../../inc/rodape.php"
-        ?>
-    </footer>
-</body>
+            <main>
+                <form action="../../../back-end/verificacaoLogin.php" method="get">
+                    <label for="name">Nome:</label>
+                    <input type="text" name="name" id="name">
+                    <input type="submit" value="Enviar">
+                </form>
+            </main>
 
-</html>
+            <footer>
+                <?php
+                    include "../../../inc/rodape.php"
+                ?>
+            </footer>
+        </body>
+
+        </html>
