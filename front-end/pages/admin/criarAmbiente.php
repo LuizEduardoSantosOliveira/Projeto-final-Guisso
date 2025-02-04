0e405b238b99c07d5a9436cf45ef580811985247
Lucas1313136 <lucasprates2a@gmail.com>
2025-02-04 08:55:37 -0300

    código de criar ambiente feito e pequenas alterções em criar categoria

--- front-end/pages/admin/criarAmbiente.php
+++ front-end/pages/admin/criarAmbiente.php
@@ -7,6 +7,33 @@
     <title>Criação de ambiente</title>
 </head>
 <body>
+    <header>
+        <?php
+            include "../../../inc/validacao.php";
+            include "../../../inc/cabecalho.php";
+        ?>
+    </header>
     
+    <main>
+        <form action="../../../back-end/salvarAmbiente.php" method="get">
+            <fieldset>
+                <legend>Criar Ambiente</legend>
+
+                <label for="nome">Nome do Ambiente:</label>
+                <input type="text" name="nome" id="nome"><br><br>   
+
+                <label for="descricao">Descrição do Ambiente:</label><br>
+                <textarea name="descricao" id="descricao" cols="80" rows="5"></textarea><br><br>
+
+                <input type="submit" value="Enviar">
+            </fieldset>
+        </form>
+    </main>
+
+    <footer>
+        <?php
+            include "../../../inc/rodape.php";
+        ?>
+    </footer>
 </body>
 </html>
