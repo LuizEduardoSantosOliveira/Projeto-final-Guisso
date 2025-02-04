a6703af0477b98ce3ca6b47bb51e3015166b7a49
LuizEduardoSantosOliveira <luizeduardo123457890@gmail.com>
2025-02-04 09:07:27 -0300

    remoção do codigo

--- front-end/pages/usuario/todasReservas.php
+++ front-end/pages/usuario/todasReservas.php
@@ -1,15 +1,22 @@
-<?php
-include './class/rb.php';
-include '../inc/validacaodata.php';
-include '../inc/validacao.php';
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reservas do Usuario</title>
+</head>
+<body>
+    <header>
+        inclu
+    </header>
 
+    <main>
 
-// Verifica se o usuário está logado
-if (!isset($_SESSION['name'])) {
-    header('Location: login.php');
-    exit();
-}
+    </main>
 
-// Busca todas as reservas com seus usuários relacionados
-$reservas = R::findAll('reserva');
-?>
+    <footer>
+
+    </footer>
+</body>
+</html>
