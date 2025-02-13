d986b2fe68b6f56f350cd392bce3e88d592fe2a7
Lucas1313136 <lucasprates2a@gmail.com>
2025-02-13 16:58:14 -0300

    configuração básica da página cadastrar usuário

--- front-end/pages/admin/cadastrarUsuario.php
+++ front-end/pages/admin/cadastrarUsuario.php
@@ -7,6 +7,35 @@
     <title>Cadastro de usuário</title>
 </head>
 <body>
-    
+    <header>
+        <?php
+            include "../../../inc/validacao.php";
+            include "../../../inc/cabecalho.php";
+        ?>
+    </header>
+
+    <main>
+        <form action="../../../back-end/salvarUsuario.php" method="get">
+            <fieldset>
+                <legend>Cadastrar Usuário</legend>
+                <label for="username">Nome do Usuário:</label>
+                <input type="text" name="username" id="username"><br><br>
+
+                <label for="email">Email:</label>
+                <input type="text" name="email" id="email"><br><br>
+
+                <label for="password">Senha:</label>
+                <input type="password" name="pwd" id="pwd" ocult><br><br>
+
+                <input type="submit" value="Enviar">
+            </fieldset>
+        </form>
+    </main>
+
+    <footer>
+        <?php
+            include "../../../inc/rodape.php";
+        ?>
+    </footer>
 </body>
 </html>
