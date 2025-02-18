4fd119c718b0d2af67db70ba000f48e73ff09d53
Lucas1313136 <lucasprates2a@gmail.com>
2025-02-18 11:20:07 -0300

    correção de caminho para fazer reserva em home"

--- front-end/pages/usuario/home.php
+++ front-end/pages/usuario/home.php
@@ -16,7 +16,7 @@
         include "../../../inc/validacao.php";
         ?>
         <nav>
-            <li><a href="calendario.php">Fazer reserva</a></li>
+            <li><a href="ambiente.php">Fazer reserva</a></li>
             <li><a href="reservasUsuario.php">Minhas reservas</a></li>
             <li><a href="sobreNos.php">Sobre Nós</a></li>
             <li><a href="todasReservas.php">Reservas no sistema</a></li>
