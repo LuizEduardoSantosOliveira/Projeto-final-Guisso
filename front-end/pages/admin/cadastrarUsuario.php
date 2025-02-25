7bb3deb6ecce3315ae13dbbbf00433c33c997126
LuizEduardo <156156838+LuizEduardoSantosOliveira@users.noreply.github.com>
2025-02-25 15:00:12 -0300

    adicionado as classes do css na pagina de cadastro de usuário

--- front-end/pages/admin/cadastrarUsuario.php
+++ front-end/pages/admin/cadastrarUsuario.php
@@ -15,7 +15,7 @@
     include "../../../inc/cabecalho.php";
     include "../../../inc/validacaoAdmin.php";
     include "../../../inc/validacao.php";
-    if(isset($_GET['id'])){
+    if (isset($_GET['id'])) {
         include "../../../back-end/buscarFuncoes.php";
     }
 
@@ -26,54 +26,57 @@
 </header>
 
 <main>
-    <form action="../../../back-end/verificacaoUsuario.php" method="get">
-        <label for="name">Nome</label>
-        <?php
-            if(isset($_GET['id'])){
+    <div class="admin-form-container">
+        <form class="admin-form" action="../../../back-end/verificacaoUsuario.php" method="get">
+            <h1>Cadastro de usuário</h1>
+            <label for="name">Nome</label>
+            <?php
+            if (isset($_GET['id'])) {
                 $usuario = buscarUsuario($_GET['id']);
                 echo '<input type="hidden" name="id" id="id" value="' . $_GET['id'] . '">';
             }
-            if(isset($_GET['id'])){
-                echo '<input type="text" name="name" id="name" value="'. $usuario->nome .'">';
-            }else{
+            if (isset($_GET['id'])) {
+                echo '<input type="text" name="name" id="name" value="' . $usuario->nome . '">';
+            } else {
                 echo '<input type="text" name="name" id="name">';
             }
-        ?>
-        
-        <label for="email">Email</label>
-        <?php
-            if(isset($_GET['id'])){
-                echo '<input type="email" name="email" id="email" value="'. $usuario->email .'" pattern=".+@(gmail\.com|outlook\.com|hotmail\.com|live\.com|msn\.com)" size="30" required>';
-            }else{
+            ?>
+
+            <label for="email">Email</label>
+            <?php
+            if (isset($_GET['id'])) {
+                echo '<input type="email" name="email" id="email" value="' . $usuario->email . '" pattern=".+@(gmail\.com|outlook\.com|hotmail\.com|live\.com|msn\.com)" size="30" required>';
+            } else {
                 echo '<input type="email" name="email" id="email" pattern=".+@(gmail\.com|outlook\.com|hotmail\.com|live\.com|msn\.com)" size="30" required>';
             }
-        ?>
+            ?>
 
-        <label for="password">Senha:</label>
-        <?php
-            if(isset($_GET['id'])){
-                echo '<input type="password" name="password" id="password" value="'. $usuario->senha .'" required>';
-            }else{
+            <label for="password">Senha:</label>
+            <?php
+            if (isset($_GET['id'])) {
+                echo '<input type="password" name="password" id="password" value="' . $usuario->senha . '" required>';
+            } else {
                 echo '<input type="password" name="password" id="password" required>';
             }
-        ?>
+            ?>
 
-        <label for="type">Tipo</label>
-        <?php
-            if(isset($_GET['id'])){
-                echo '<select name="type" id="type" value="'. $usuario->tipo .'">
+            <label for="type">Tipo</label>
+            <?php
+            if (isset($_GET['id'])) {
+                echo '<select name="type" id="type" value="' . $usuario->tipo . '">
                      <option value="admin">Admin</option>
                      <option value="user">Usuário</option>
                      </select>';
-            }else{
+            } else {
                 echo '<select name="type" id="type">
                      <option value="admin">Admin</option>
                      <option value="user">Usuário</option>
                      </select>';
             }
-        ?>
+            ?>
 
-        <button type="submit">Enviar</button>
+            <button class="submit" type="submit">Enviar</button>
+    </div>
 
 </main>
 
